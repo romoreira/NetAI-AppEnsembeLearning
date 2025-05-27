7b92b110b619bde7ba037b2e93fb723690494ea9
romoreira <rodrigo.moreira206@gmail.com>
2025-05-27 16:41:35 -0300

    first benchmarking

--- el-mqtt/run_el-mqtt.sh
+++ el-mqtt/run_el-mqtt.sh
@@ -6,9 +6,9 @@ pkill -f "server_el-mqtt.py"
 sleep 4
 
 
-python3 client_el-mqtt.py --broker 10.103.117.206 --port 1883 --topic probs --model_name alexnet --optimizer sgd --lr 0.001   --epochs 1   --batch_size 64 --client_id 1 --ensemble_method pso_stacking &
-python3 client_el-mqtt.py --broker 10.103.117.206 --port 1883 --topic probs --model_name resnet18 --optimizer sgd --lr 0.001   --epochs 1   --batch_size 64 --client_id 2 --ensemble_method pso_stacking & 
-python3 client_el-mqtt.py --broker 10.103.117.206 --port 1883 --topic probs --model_name mobilenet_v2 --optimizer sgd --lr 0.001   --epochs 1   --batch_size 64 --client_id 3 --ensemble_method pso_stacking & 
+python3 client_el-mqtt.py --broker 10.103.117.206 --port 1883 --topic probs --model_name alexnet --optimizer sgd --lr 0.001   --epochs 20   --batch_size 64 --client_id 1 --ensemble_method pso_stacking &
+python3 client_el-mqtt.py --broker 10.103.117.206 --port 1883 --topic probs --model_name resnet18 --optimizer sgd --lr 0.001   --epochs 20   --batch_size 64 --client_id 2 --ensemble_method pso_stacking & 
+python3 client_el-mqtt.py --broker 10.103.117.206 --port 1883 --topic probs --model_name mobilenet_v2 --optimizer sgd --lr 0.001   --epochs 20   --batch_size 64 --client_id 3 --ensemble_method pso_stacking & 
 
 
 
